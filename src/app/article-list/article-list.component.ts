3f4d4d929073b159d74e39050f93a1319c1677b6
ssliteris <ssliteris@deloitte.com>
2017-09-13 23:16:02 +0300

    cleanup

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,11 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Article} from "../classes/article";
 
-const ARTICLES: Article[] = [
-    {id: 0, name: 'first', text: 'first article text'},
-    {id: 1, name: 'second', text: 'second article text'}
-];
-
 @Component({
     selector: 'app-article-list',
     templateUrl: './article-list.component.html',
@@ -13,8 +8,11 @@ const ARTICLES: Article[] = [
 })
 export class ArticleListComponent implements OnInit {
     
-    articles = ARTICLES;
-    selectedArticle: Article;
+    public articles: Article[] = [
+        {id: 0, name: 'first', text: 'first article text'},
+        {id: 1, name: 'second', text: 'second article text'}
+    ];
+    public selectedArticle: Article;
     
     onSelect(article: Article): void {
         this.selectedArticle = article;
